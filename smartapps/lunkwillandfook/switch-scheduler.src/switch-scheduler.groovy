363103e89ca2fc617e0c9c8f92f02aee584159f0
Jeremy Huckeba <jshuckeba@live.com>
2015-09-28 16:30:56 -0400

    Adding functionality

--- smartapps/lunkwillandfook/switch-scheduler.src/switch-scheduler.groovy
+++ smartapps/lunkwillandfook/switch-scheduler.src/switch-scheduler.groovy
@@ -25,14 +25,53 @@ definition(
 
 
 preferences {
-	section("Switch settings") {
-		input(name: "isRunForNext24Hours", type: "bool", title: "Run for next 24 hours?")
-        input(name: "selectedSwitch", type: "capability:switch", title: "Select a switch", required: true, multiple: false)
-		input(name: "starttime", type: "time", title: "Turn on at", required: false)
-		input(name: "endtime", type: "time", title: "Turn off at", required: false)
-        input(name: "onlyInModes", type: "mode", title: "Run schedule only if in modes?", required: false, multiple: true)
-        input(name: "isTriggerOnModeChange", type: "bool", title: "Trigger the switch on or off when the mode changes.")
+	page(name: "page1", title: "Configuration...", uninstall: true, install: false, nextPage: "page2") {
+    	section("24 Hour Mode") {
+        	input(name: "isRunForNext24Hours", type: "bool", title: "Run for next 24 hours?")
+        }
+		section("Switch settings") {
+        	input(name: "selectedSwitch", type: "capability:switch", title: "Select the switches to trigger...", required: true, multiple: true)
+			def timeLabel = timeIntervalLabel()
+			href(name: "timeIntervalInput", title: "Only during a certain time:", description: timeLabel ?: "Tap to set", state: timeLabel ? "complete" : null)
+			input(name: "days", type: "enum", title: "Only on certain days of the week:", multiple: true, required: false, options: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"])
+			input(name: "modes", type: "mode", title: "Only when mode is:", multiple: true, required: false)
+        	input(name: "isTriggerOnModeChange", type: "bool", title: "Always trigger when mode changes?")
+		}
 	}
+    page(name: "page2")
+	page(name: "timeIntervalInput", title: "Only during a certain time...")
+    {
+		section
+        {
+			input "startingTime", "time", title: "Starting", required: false
+			input "endingTime", "time", title: "Ending", required: false
+		}
+	}
+}
+
+def page2() {
+	dynamicPage(name: "page2", title: "Switch Levels", uninstall: true, install: true) {
+    	section() {
+        	def i = 0
+            selectedSwitches.each { selectedSwitch ->
+            	if(i < 20) {
+                	def inputName = "switchLevel$i"
+                    input(name: inputName, type: "enum", title: selectedSwitch.label, multiple: false, required: true, options: getSwitchLevelOptions(selectedSwitch))
+                    i++
+                }
+            }
+        }
+    }
+}
+
+private getSwitchLevelOptions(selectedSwitch) {
+	if(selectedSwitch.hasCommand("setLevel")) {
+    	// dimmable switch options
+        return ["Off", "5%", "10%", "15%", "20%", "25%", "30%", "35%", "40%", "45%", "50%", "55%", "60%", "65%", "70%", "75%", "80%", "85%", "90%", "95%", "On" ]
+    } else {
+    	// relay switch options
+        return ["Off", "On" ]
+    }
 }
 
 def installed() {
@@ -53,4 +92,7 @@ def initialize() {
     
 }
 
-// TODO: implement event handlers
+private timeIntervalLabel()
+{
+	(startingTime && endingTime) ? hhmm(startingTime) + "-" + hhmm(endingTime, "h:mm a z") : ""
+}
