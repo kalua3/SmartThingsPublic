4bab94b9e8e01c54b6163d2975bb4c8fe0897585
Jeremy Huckeba <jshuckeba@live.com>
2015-12-16 22:31:36 -0500

    Fixed bug

--- smartapps/lunkwillandfook/mode-master.src/mode-master.groovy
+++ smartapps/lunkwillandfook/mode-master.src/mode-master.groovy
@@ -15,7 +15,7 @@ definition(
     iconX3Url: "http://cdn.device-icons.smartthings.com/Kids/kids5-icn@3x.png")
 
 preferences {
-	page(name: "page1", title: "Welcome", nextPage: "page2", uninstall: true) {
+	page(name: "page1", title: "Welcome", uninstall: true, install: true) {
 		section() {
 			paragraph "Welcome. This app will let you configure on or more trigger modes and a target mode so that the mode is automatically set to the target whenever it is changed to one of the triggers." 
     		label title: "Assign a name", required: false
