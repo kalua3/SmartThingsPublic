a3459cf3b13bb697d54b797b47417d022e6708af
Jeremy Huckeba <jshuckeba@live.com>
2015-12-16 23:44:19 -0500

    Added timer to fix bug where mode had not yet changed.

--- smartapps/lunkwillandfook/mode-master.src/mode-master.groovy
+++ smartapps/lunkwillandfook/mode-master.src/mode-master.groovy
@@ -4,6 +4,9 @@
  *  Copyright 2015 Jeremy Huckeba
  *
  */
+
+import groovy.time.TimeCategory
+
 definition(
     name: "Mode Master",
     namespace: "LunkwillAndFook",
@@ -20,7 +23,7 @@ preferences {
 			paragraph "Welcome. This app will let you configure on or more trigger modes and a target mode so that the mode is automatically set to the target whenever it is changed to one of the triggers." 
     		label title: "Assign a name", required: false
 			input(name: "triggerModes", type: "mode", title: "When mode is changed to", multiple: true, required: true)
-			input(name: "targetMode", type: "mode", title: "Automatically change it to this mode", multiple: false, required: true)
+			input(name: "modeToSet", type: "mode", title: "Automatically change it to this mode", multiple: false, required: true)
         }
     }
 }
@@ -42,10 +45,19 @@ def initialize() {
 	subscribe(location, "mode", modeChangeHandler)
 }
 
-def modeChangeHandler(evt){
-	log.debug "Updated with mode: ${evt}"
-	if(triggerModes.contains(evt.value)) {
-		location.mode = targetMode
-		log.debug "Mode automatically changed to ${targetMode}"
+def modeChangeHandler(evt) {
+	def currentMode = evt.value
+	log.debug "Updated with mode: ${currentMode}"
+	if(triggerModes.contains(currentMode)) {
+		use(TimeCategory) {
+			def after5Seconds = new Date() + 5.seconds
+			runOnce(after5Seconds, updateModeHandler)
+		}
 	}
+}
+
+def updateModeHandler() {
+	log.debug "Target mode is ${modeToSet}"
+	location.setMode(modeToSet)
+	log.debug "Mode automatically changed to ${modeToSet}"
 }
